27c2cdf5ff4d0bf00dc12c5dfba5cb74b1d3423f
Nail Shakirov <shakirov_nail@bk.ru>
2020-06-23 17:44:06 +0500

    Updates

---
+++ precache-manifest.f96d2f01cc49fa6efa2ab8cb93413462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5580b1ccab1ae6fa896f0005e2725f80",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "61a5d8b75987e4d23e11",
+    "url": "/todo-redux/static/css/main.c9dfae4b.chunk.css"
+  },
+  {
+    "revision": "dfc108e6aeaadc605225",
+    "url": "/todo-redux/static/js/2.94069d32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-redux/static/js/2.94069d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a5d8b75987e4d23e11",
+    "url": "/todo-redux/static/js/main.30a73879.chunk.js"
+  },
+  {
+    "revision": "85a6f7e3b4d8fc7c9a38",
+    "url": "/todo-redux/static/js/runtime-main.c0227ecf.js"
+  }
+]);
